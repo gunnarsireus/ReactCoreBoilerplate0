8cbe5080d6dde2fb1f48f03c5d77991c27e7b6ab
gunnarsireus <gunnar.sireus@gmail.com>
2019-03-11 06:08:47 +0100

    Add project files.

---
+++ ReactCoreBoilerplate0/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace ReactCoreBoilerplate0
+{
+    public static class Constants
+    {
+        public static string AuthorizationCookieKey => "Auth";
+        public static string HttpContextServiceUserItemKey => "ServiceUser";
+    }
+}
